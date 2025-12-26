16f6c480821273aef91c2fc20f73f10458ad916a
Hanson Emmanuel <hansonemma398@gmail.com>
2025-12-26 14:34:30 +0100

    Added functionality to the add playlist button in the playlists page

--- src/Pages/PlayListsPage.jsx
+++ src/Pages/PlayListsPage.jsx
@@ -4,7 +4,7 @@ import { useCallback } from 'react';
 import { usePlaylistContext } from '../Contexts/PlaylistContext.jsx';
 
 const PlayLists = () => {
-    const { playlists } = usePlaylistContext();
+    const { playlists, setIsOpenModal, setView } = usePlaylistContext();
 
     const renderPlaylists = useCallback(() => {
         if (playlists.length === 0) {
@@ -44,7 +44,10 @@ const PlayLists = () => {
                     </p>
                 </div>
                 
-                <button className="flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white rounded-full text-xs font-bold hover:bg-indigo-700 transition-all active:scale-95 shadow-lg shadow-indigo-100">
+                <button 
+                    className="flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white rounded-full text-xs font-bold hover:bg-indigo-700 transition-all active:scale-95 shadow-lg shadow-indigo-100"
+                    onClick={() => { setView('create'); setIsOpenModal(true); }}
+                >
                     <Plus size={18} />
                     <span className="hidden sm:inline">New Playlist</span>
                 </button>
